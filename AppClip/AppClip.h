98cfd248f3128a82bd516d0e30aa0dab3082fe6b
nixzhu <zhuhongxu@gmail.com>
2017-07-13 09:58:15 +0800

    init

---
+++ AppClip/AppClip.h
@@ -0,0 +1,19 @@
+//
+//  AppClip.h
+//  AppClip
+//
+//  Created by nixzhu on 2017/7/13.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppClip.
+FOUNDATION_EXPORT double AppClipVersionNumber;
+
+//! Project version string for AppClip.
+FOUNDATION_EXPORT const unsigned char AppClipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppClip/PublicHeader.h>
+
+
---
+++ IcePack/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IcePack
+//
+//  Created by nixzhu on 2017/7/13.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
