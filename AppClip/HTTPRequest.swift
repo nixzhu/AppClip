6c26ceddb2afa980fd2b03f4a37d6d5bbf8fd163
nixzhu <zhuhongxu@gmail.com>
2017-07-13 11:30:42 +0800

    add HTTPRequest

---
+++ AppClip/HTTPRequest.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPRequest.swift
+//  AppClip
+//
+//  Created by nixzhu on 2017/7/13.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+import Foundation
+
+class HTTPRequest {
+
+    var method: String = ""
+    var path: String = ""
+    var queryParams: [(String, String)] = []
+
+    init() {
+    }
+}
